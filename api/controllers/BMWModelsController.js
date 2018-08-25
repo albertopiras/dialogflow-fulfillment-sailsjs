766a216ec6464160d38991464e4fca2182db542b
Alberto Piras <a.piras.ict@gmail.com>
2018-08-25 19:21:03 +0200

    Updating response with specific model

--- api/controllers/BMWModelsController.js
+++ api/controllers/BMWModelsController.js
@@ -1,34 +1,56 @@
 var chance = require('chance').Chance();
 var _ = require('lodash');
 
+
+
+function createCard(modelId) {
+  let model = ModelsService.getModel(modelId);
+  // console.log("--->" + JSON.stringify(model));
+
+  return {
+    "card": {
+      "title": model.model_name,
+      "subtitle": model.model_series,
+      "imageUri": model.model_img_url,
+      "buttons": [
+        {
+          "text": "see more on",
+          "postback": "https://www.bmw.com/en/bmw-models.html"
+        }
+      ]
+    }
+  }
+
+
+  //TO-DO:  add logic to show all models of a specific serie
+  
+}
+
 module.exports = {
 
   list: function (req, res) {
 
     var selectedModel = req.body.queryResult.parameters.BMW_models;
 
-    console.log(selectedModel);
+    console.log(selectedModel[0]);
 
-    var response =
+    if (selectedModel) {
 
-      {
-        "fulfillmentText": "Info about your selection",
-        "fulfillmentMessages": [
-          {
-            "card": {
-              "title": "Bmw series 1",
-              "subtitle": "card text",
-              "imageUri": "https://www.bmw.com/content/dam/bmw/marketBMWCOM/bmw_com/categories/all-models/1-series/3-and-5-door/2017/bmw-1series-00-teaser-hd-cosy.jpg?",
-              "buttons": [
-                {
-                  "text": "button text",
-                  "postback": "https://assistant.google.com/"
-                }
-              ]
-            }
-          }
-        ]
+      var response =
+
+        {
+          "fulfillmentText": "Info about your selection",
+          "fulfillmentMessages": [
+            createCard(selectedModel[0])
+          ]
+        }
+    } else {
+      var response = {
+        "fulfillmentText": "Webhook : Select a BMW model please"
       }
+    }
+
+
     return res.ok(response);
 
   },
@@ -37,11 +59,8 @@ module.exports = {
     var temp = ModelsService.getUser(req.param('id'));
     var response =
       {
-        "data": {
-         "car":"TO-DO"
-        }
-      }
 
+      }
     return res.ok(response);
   }
 
--- api/services/ModelsService.js
+++ api/services/ModelsService.js
@@ -2,29 +2,42 @@ var chance = require('chance').Chance();
 
 
 var bmw_models = [{
-  model_id : chance.guid(),
+  model_id : 'model1',
   model_series: "Series 1",
-  model_name: "Bmw 1 Series"
+  model_name: "Bmw 1 Series",
+  model_img_url:"https://www.bmw.com/content/dam/bmw/marketBMWCOM/bmw_com/categories/all-models/1-series/3-and-5-door/2017/bmw-1series-00-teaser-hd-cosy.jpg"
 },
 {
-  model_id : chance.guid(),
+  model_id : 'model2',
+  model_series: "Series 2"
+},
+{
+  model_id : 'model2coupe',
   model_series: "Series 2",
-  model_name: "The BMW 2 Series Coupé"
+  model_name: "The BMW 2 Series Coupé",
+  model_img_url:"https://www.bmw.com/content/dam/bmw/marketBMWCOM/bmw_com/categories/all-models/2-series/coupe/2017/bmw-2series-coupe-00-teaser-hd-cosy.jpg"
 },
 {
-  model_id : chance.guid(),
+  model_id : 'model2activetourer',
   model_series: "Series 2",
-  model_name: "The BMW 2 Series Active Tourer"
+  model_name: "The BMW 2 Series Active Tourer",
+  model_img_url:"https://www.bmw.com/content/dam/bmw/marketBMWCOM/bmw_com/categories/all-models/2-series/active-tourer/2018/bmw-2series-activetourer-2018-00-teaser-hd-cosy.jpg"
 },
 {
-  model_id : chance.guid(),
-  model_series: "Series 3",
-  model_name: "The BMW 3 Series coupe"
+  model_id : 'model3',
+  model_series: "Series 3"
 },
 {
-  model_id : chance.guid(),
+  model_id : 'model3coupe',
   model_series: "Series 3",
-  model_name: "The BMW 3 Series Touring"
+  model_name: "The BMW 3 Series coupe",
+  model_img_url:"https://www.bmw.com/content/dam/bmw/marketBMWCOM/bmw_com/categories/all-models/3-series/sedan/2015/bmw-3series-sedan-00-teaser-hd_cosy.jpg"
+},
+{
+  model_id : 'model3touring',
+  model_series: "Series 3",
+  model_name: "The BMW 3 Series Touring",
+  model_img_url:"https://www.bmw.com/content/dam/bmw/marketBMWCOM/bmw_com/categories/all-models/3-series/touring/2015/bmw-3series-touring-00-teaser-hd-cosy.jpg"
 }];
 
 
@@ -35,9 +48,10 @@ module.exports = {
   },
 
 
-  getModel: function (id) {
+  getModel: function (modelId) {
+    console.log('get model ' + modelId);
     return _.find(this.getModels(), function (item) {
-      return item.model_id === id;
+      return item.model_id === modelId;
     });
   }
 
